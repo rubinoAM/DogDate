4a331049b2e51bde779ddf6dfb0ebc765cdc208a
Michael Rubino <parkcathedralgrace@gmail.com>
2019-05-14 14:05:42 -0400

    Value Model & DataContext

--- DogDate.API/Data/DataContext.cs
+++ DogDate.API/Data/DataContext.cs
@@ -1,9 +1,12 @@
+using DogDate.API.Models;
 using Microsoft.EntityFrameworkCore;
 
 namespace DogDate.API.Data
 {
     public class DataContext : DbContext
     {
-        
+        public DataContext(DbContextOptions<DataContext> options) : base(options){ }
+
+        public DbSet<Value> Values { get; set; }
     }
 }
