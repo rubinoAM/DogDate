02a4e7bf274ae4135d36e83a5b3135fd2488bba8
Michael Rubino <parkcathedralgrace@gmail.com>
2019-05-14 15:36:40 -0400

    Setting up HTTP Request in Angular

---
+++ DogDate-SPA/src/value/value.component.ts
@@ -0,0 +1,29 @@
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+@Component({
+  selector: 'app-value',
+  templateUrl: './value.component.html',
+  styleUrls: ['./value.component.css']
+})
+export class ValueComponent implements OnInit {
+  values: any;
+
+  constructor(private http: HttpClient) { }
+
+  ngOnInit() {
+    this.getValues();
+  }
+
+  getValues() {
+    this.http.get('http://localhost:8889/api/values')
+      .subscribe(
+        (resp) => {
+          this.values = resp;
+        },
+        (err) => {
+          console.log(err);
+        }
+      );
+  }
+}
