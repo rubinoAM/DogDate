f8cce280e428dbb6c182e88dcb1aadaa4eaec889
Michael Rubino <parkcathedralgrace@gmail.com>
2019-05-16 19:09:59 -0400

    Repository Interface

---
+++ DogDate.API/Data/IAuthRepository.cs
@@ -0,0 +1,12 @@
+using System.Threading.Tasks;
+using DogDate.API.Models;
+
+namespace DogDate.API.Data
+{
+    public interface IAuthRepository //Interface names start with capital 'I'
+    {
+        Task<User> Register(User user, string password);
+        Task<User> Login(string username, string password);
+        Task<bool> UserExists(string username);
+    }
+}
