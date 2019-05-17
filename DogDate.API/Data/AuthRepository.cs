bcacd04138739bf648777699e1d2e9d51eb76c80
Michael Rubino <parkcathedralgrace@gmail.com>
2019-05-17 18:43:22 -0400

    Register Method

--- DogDate.API/Data/AuthRepository.cs
+++ DogDate.API/Data/AuthRepository.cs
@@ -10,9 +10,26 @@ namespace DogDate.API.Data
         {
             this._context = context;
         }
-        public Task<User> Register(User user, string password)
+        public async Task<User> Register(User user, string password)
         {
-            throw new System.NotImplementedException();
+            byte[] passwordHash, passwordSalt;
+            CreatePasswordHash(password, out passwordHash, out passwordSalt);
+
+            user.PasswordHash = passwordHash;
+            user.PasswordSalt = passwordSalt;
+
+            await _context.Users.AddAsync(user);
+            await _context.SaveChangesAsync();
+
+            return user;
+        }
+        private void CreatePasswordHash(string password, out byte[] passwordHash, out byte[] passwordSalt)
+        {
+            using(var hmac = new System.Security.Cryptography.HMACSHA512())
+            {
+                passwordSalt = hmac.Key;
+                passwordHash = hmac.ComputeHash(System.Text.Encoding.UTF8.GetBytes(password));
+            }
         }
         public Task<User> Login(string username, string password)
         {
