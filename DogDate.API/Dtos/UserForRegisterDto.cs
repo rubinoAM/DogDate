7105b79acaefcb2af978a7c60ab996f9b33d4c60
Michael Rubino <parkcathedralgrace@gmail.com>
2019-05-18 18:26:54 -0400

    Requirements for username and password

--- DogDate.API/Dtos/UserForRegisterDto.cs
+++ DogDate.API/Dtos/UserForRegisterDto.cs
@@ -1,8 +1,15 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace DogDate.API.Dtos
 {
     public class UserForRegisterDTO
     {
+        [Required]
+        [StringLength(16,MinimumLength = 6,ErrorMessage = "You must specify a username between 6 and 16 characters.")]
         public string Username { get; set; }
+        
+        [Required]
+        [StringLength(25,MinimumLength = 8,ErrorMessage = "You must specify a password between 8 and 25 characters.")]
         public string Password { get; set; }
     }
 }
