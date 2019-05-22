7d0cbd260b9386f404cc9d1d9819cbd8f11dd47a
Michael Rubino <parkcathedralgrace@gmail.com>
2019-05-21 22:20:58 -0400

    Working on navbar

--- DogDate-SPA/src/app/app.module.ts
+++ DogDate-SPA/src/app/app.module.ts
@@ -2,20 +2,24 @@ import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
+import { FlexLayoutModule } from '@angular/flex-layout';
 
 import { AppComponent } from './app.component';
 import { ValueComponent } from '../value/value.component';
 import { MaterialModule } from './material.module';
+import { NavbarComponent } from './navbar/navbar.component';
 
 @NgModule({
    declarations: [
       AppComponent,
-      ValueComponent
+      ValueComponent,
+      NavbarComponent
    ],
    imports: [
       BrowserModule,
       HttpClientModule,
       BrowserAnimationsModule,
+      FlexLayoutModule,
       MaterialModule
    ],
    providers: [],
