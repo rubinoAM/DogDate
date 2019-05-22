aeea0cbb6c1130f51adcbcc2807cdc9e4ad99dbd
Michael Rubino <parkcathedralgrace@gmail.com>
2019-05-21 20:43:09 -0400

    Material module

--- DogDate-SPA/src/app/app.module.ts
+++ DogDate-SPA/src/app/app.module.ts
@@ -1,9 +1,11 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 import { ValueComponent } from '../value/value.component';
+import { MaterialModule } from './material.module';
 
 @NgModule({
    declarations: [
@@ -12,7 +14,9 @@ import { ValueComponent } from '../value/value.component';
    ],
    imports: [
       BrowserModule,
-      HttpClientModule
+      HttpClientModule,
+      BrowserAnimationsModule,
+      MaterialModule
    ],
    providers: [],
    bootstrap: [
