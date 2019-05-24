b097e2b91c95e47d54fb3970a8583936baf28cac
Michael Rubino <parkcathedralgrace@gmail.com>
2019-05-23 22:19:48 -0400

    Auth service

--- DogDate-SPA/src/app/navbar/_services/auth.service.ts
+++ DogDate-SPA/src/app/navbar/_services/auth.service.ts
@@ -1,11 +1,25 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { map } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
+  baseUrl = 'http://localhost:8889/api/auth/';
 
 constructor(private http: HttpClient) { }
+
+login(model: any) {
+  return this.http.post(this.baseUrl + 'login', model)
+    .pipe(
+      map((resp: any) => {
+        const user = resp;
+        if(user){
+          localStorage.setItem('token', user.token);
+        }
+      })
+    );
+}
 
 }
