6e06a454bccaacb33b69abe0978b343ad44c6f83
Michael Rubino <parkcathedralgrace@gmail.com>
2019-05-23 23:39:53 -0400

    Register component

--- DogDate-SPA/src/app/register/register.component.ts
+++ DogDate-SPA/src/app/register/register.component.ts
@@ -6,10 +6,19 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./register.component.css']
 })
 export class RegisterComponent implements OnInit {
+  model: any = {};
 
   constructor() { }
 
   ngOnInit() {
+  }
+
+  register(){
+    console.log(this.model);
+  }
+
+  cancel(){
+    console.log('BARK BARK');
   }
 
 }
